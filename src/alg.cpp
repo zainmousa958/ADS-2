ec8254e2ec15ffc3a2df2c54d17f950d013fb58b
zainmousa958 <80972355+zainmousa958@users.noreply.github.com>
2021-04-10 22:01:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,60 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-  return 0;
-}
+        int count = 0;
+                for (int i = 0; i < len-1; i++) {
+                for (int j = i + 1; j < len; j++) {
+        if (arr[i] + arr[j] == value)
+                k++;
+        }
+        }
+        if ( count > 0 )
+                return count;
+        else
+                return 0;
+        }
 
 int countPairs2(int *arr, int len, int value) {
-  return 0;
-}
+        int count = 0;
+                for (int j = len - 1; j > 0; j--) {
+                for (int i = 0; i < j; i++) {
+                if ((arr[i] + arr[j]) == value)
+                count++;
+        }
+        }
+        if ( count > 0 )
+                return count;
+        else
+                return 0;
+        }
+
 
 int countPairs3(int *arr, int len, int value) {
-  return 0;
-}
+        int count = 0;
+                for (int i = 0; i < len-1; i++) {
+        int left = i+1;
+        int right = len;
+        int res = value - arr[i];
+                while (left < right) {
+        int center = (left + right)/2;
+                if (arr[center] == res) {
+                while ((arr[center-1] == res) && (center > left)) {
+        center--;
+        }
+                while (arr[center] == res) {
+        center++;
+        count++;
+        }
+        break;
+        } else if (arr[center] > res) {
+        right = center;
+        } else {
+        left = center+1;
+        }
+        }
+        }
+        if ( count > 0 )
+                return count;
+        else
+                return 0;
+        }
